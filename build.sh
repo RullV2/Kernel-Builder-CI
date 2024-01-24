3b8dc95ffe09179a3c39b13a8cb2db50e3b2a477
Ismasrull <157438148+Ismasrull@users.noreply.github.com>
2024-01-24 18:24:09 +0700

    Encryp

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ START=$(date +"%s")
 KERNEL_DIR=$(pwd)
 NAME_KERNEL_FILE="$1"
 chat_id="$TG_CHAT"
-token="6118183207:AAGFBiujEbR8dFT9n5NPXB5HwRQ22TFnkzU"
+token="$TG_TOKEN"
 
 #INFROMATION NAME KERNEL
 export KBUILD_BUILD_USER=$(grep kbuild_user $NAME_KERNEL_FILE | cut -f2 -d"=" )
